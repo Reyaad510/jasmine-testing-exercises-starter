543eeff3e02f07d380938e59690ba9b390a06aba
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2024-01-10 22:22:20 -0500

    Further Study Step One Complete

--- tip-pool/servers.test.js
+++ tip-pool/servers.test.js
@@ -1,17 +1,28 @@
-describe("Servers test (with setup and tear-down)", function() {
+describe("Servers test (with setup and tear-down)", function () {
   beforeEach(function () {
     // initialization logic
-    serverNameInput.value = 'Alice';
+    serverNameInput.value = "Alice";
   });
 
-  it('should add a new server to allServers on submitServerInfo()', function () {
+  it("should add a new server to allServers on submitServerInfo()", function () {
     submitServerInfo();
 
     expect(Object.keys(allServers).length).toEqual(1);
-    expect(allServers['server' + serverId].serverName).toEqual('Alice');
+    expect(allServers["server" + serverId].serverName).toEqual("Alice");
   });
 
-  afterEach(function() {
+  it("should append a new table row with server name", function () {
+    submitServerInfo();
+    updateServerTable();
+
+    let serverNameUI = document.querySelector("#serverTable tbody tr td");
+    expect(serverNameUI.innerText).toEqual("Alice");
+  });
+
+  afterEach(function () {
     // teardown logic
+    serverId = 0;
+    serverTbody.innerHTML = "";
+    allServers = {};
   });
 });
