aa4e285a66d9b5afc9ddad021d5674ff5f94eb03
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2024-01-11 09:52:23 -0500

    Created remove button. Step 3 not fully completed

--- tip-pool/helpers.js
+++ tip-pool/helpers.js
@@ -1,4 +1,3 @@
-
 // accepts 'tipAmt', 'billAmt', 'tipPercent' and sums total from allPayments objects
 function sumPaymentTotal(type) {
   let total = 0;
@@ -19,8 +18,20 @@ function calculateTipPercent(billAmt, tipAmt) {
 
 // expects a table row element, appends a newly created td element from the value
 function appendTd(tr, value) {
-  let newTd = document.createElement('td');
+  let newTd = document.createElement("td");
   newTd.innerText = value;
+
+  tr.append(newTd);
+}
+
+function appendDeleteBtn(tr, type) {
+  let newTd = document.createElement("td");
+  newTd.className = "delete-button";
+  newTd.innerText = "X";
+
+  newTd.addEventListener("click", function (e) {
+    console.log(e.target.parentElement.remove());
+  });
 
   tr.append(newTd);
 }
