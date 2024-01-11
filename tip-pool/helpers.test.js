72a871a22c828a94bb0570009e1731a43b695fbd
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2024-01-10 23:30:41 -0500

    one helper.test.js, beforeEach and afterEach logic

--- tip-pool/helpers.test.js
+++ tip-pool/helpers.test.js
@@ -1,9 +1,25 @@
-describe("Helpers test (with setup and tear-down", function () {
+describe("Helpers test (with setup and tear-down)", function () {
   beforeEach(function () {
     //init
+    billAmtInput.value = 100;
+    tipAmtInput.value = 5;
+    submitPaymentInfo();
+  });
+
+  it("should calculate the tip percentage", function () {
+    expect(sumPaymentTotal("tipPercent")).toEqual(5);
   });
 
   afterEach(function () {
     //teardown logic
+    billAmtInput.value = "";
+    tipAmtInput.value = "";
+    allPayments = {};
+    paymentId = 0;
+    paymentTbody.innerHTML = "";
+    summaryTds[0].innerHTML = "";
+    summaryTds[1].innerHTML = "";
+    summaryTds[2].innerHTML = "";
+    paymentId = 0;
   });
 });
